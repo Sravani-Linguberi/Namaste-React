47fd4626a3c93a6f698bb6f62080b146636ea484
Sravani Linguberi <sravani.linguberi369@gmail.com>
2024-07-03 12:12:00 +0530

    basic layout for food ordering app

--- App.js
+++ App.js
@@ -1,51 +1,81 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import '/index.css'
 
-const myRoot = ReactDOM.createRoot(document.getElementById("root"));
-
-// creating react element in traditional way
-const text = React.createElement(
-  "h1",
-  { id: "text" },
-  "This is a react core way of creating react Element (heading)"
-);
-
-// creating react element using JSX with multiple lines
-const elementText = (
-  <div>
-    <h1>This is jsx heading</h1>
-    <h2>This is head 2</h2>
-  </div>
-);
-
-// rendeting word and title funtional comp inside Heading component
-// renders react element using {} // we can write any js code inside this curly brackets
-const HeadingComponent = () => {
-  return <div id="container">
-    {text}
-    <Title />
-    <Word />
-    <h1 className="head"> heading in funtional Component</h1>
-    <Name/>
-  </div>
+const Header = () => {
+  return (<div className="header-container">
+    <img className="app-logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGiUbMhaXl73QmffRRNIg3NhkMNuEL4ie6ag&s"/>
+    <div className="nav-container">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>contact us</li>
+        <li>cart</li>
+      </ul>
+    </div>
+  </div>)
 }
 
-const Word = function (){
+const Body = () => {
+  return (<div className="card-container">
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
+  </div>)
+}
+
+const RestaurantCard = () => {
   return (
-    <div>
-      word in function
+    <div className="restCard">
+      <img className="restImg" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/c75ff48c-b373-4733-8126-e3180cbb0fa0_344287.jpg" alt="Restaurant card" />
+      <h3 className="res-name">vara lakshmi</h3>
+      <h4> 4.5rating . 45 mins </h4>
+      <h4>South Indian</h4>
+      <h5>Filmnagar</h5>
     </div>
   )
 }
 
-// jsx with return 
-const Title = () => {
-  return (<h2>title of the page &&</h2>)
+const Search = () => {
+  return (
+    <div className="search">
+      <input />
+      search
+    </div>
+  )
 }
 
-// jsx without return
-const Name = () => (<h2>name text</h2>)
+const Footer = () => {
+  return (<div className="footer">
+    <div>copy right</div>
+    <div>about</div>
+    <div>contact us</div>
+    <div>address</div>
+  </div>)
+}
+
+const AppLoader = () => {
+  return (
+    <div > 
+      <Header />
+      <Search />
+      <Body />  
+      <Footer />
+    </div>
+  )
+}
 
 
-//rendering jsx at root
-myRoot.render(<HeadingComponent/>);
+
+
+const myRoot = ReactDOM.createRoot(document.getElementById("root"));
+myRoot.render(<AppLoader/>);
