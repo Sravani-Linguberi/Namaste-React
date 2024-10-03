f903ea0f83b5ed1f3c3ba9c539814708b84e3b87
Sravani Linguberi <sravani.linguberi369@gmail.com>
2024-10-03 15:12:02 +0530

    state lifting from child to parent. ( closing other all accordian on click or select of accordian

--- src/components/ResMenu.js
+++ src/components/ResMenu.js
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
-import { resMenuUrl } from '../utils/constants';
+import { resMenuUrl } from "../utils/constants";
 import { useParams } from "react-router-dom";
-import Menu from './Menu';
+import Menu from "./Menu";
 import Shimmer from "./Shimmer";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
-import RestaurantCategory from './RestaurantCategory';
-
+import RestaurantCategory from "./RestaurantCategory";
 
 const ResMenu = () => {
-    const {resId} = useParams();
-    const [resData, restaurantMenu]  = useRestaurantMenu(resId);
-   
-    return restaurantMenu?.length ? (
-        <>
-            <div className="font-bold text-center mx-auto my-10 text-xl" >
-                { resData?.data?.cards[0]?.card?.card?.text }
-            </div>    
-            {
-                restaurantMenu?.map(val => <RestaurantCategory key={val.card.card.title} data={val?.card?.card}/>)
-                // restaurantMenu?.map(val => <Menu key={val?.card?.info?.id} itemDetails= {val?.card}/>)
-            }
-        </>
-    ) : <> <h3>Loading menu</h3> <Shimmer/></>
-}
+  const { resId } = useParams();
+  const [resData, restaurantMenu] = useRestaurantMenu(resId);
+  const [showItem, setShowItem] = useState(0);
+
+  return restaurantMenu?.length ? (
+    <>
+      <div className="font-bold text-center mx-auto my-10 text-xl">
+        {resData?.data?.cards[0]?.card?.card?.text}
+      </div>
+      {
+        restaurantMenu?.map((val, index) => (
+          <RestaurantCategory
+            key={val.card.card.title}
+            data={val?.card?.card}
+            showItem= {showItem === index}
+            setShowItemCD = {() =>{setShowItem(index)}} //state lifting : passing callback funtion to the child and using from there to set the parent value
+          />
+        ))
+        // restaurantMenu?.map(val => <Menu key={val?.card?.info?.id} itemDetails= {val?.card}/>)
+      }
+    </>
+  ) : (
+    <>
+      {" "}
+      <h3>Loading menu</h3> <Shimmer />
+    </>
+  );
+};
 
 export default ResMenu;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import Menu from "./Menu";
 
-const RestaurantCategory = ({ data }) => {
-  console.log(data.itemCards[0].card, " RestaurantCategory", data);
-  const [isActive, setIsActive] = useState(true);
+const RestaurantCategory = ({ data , showItem, setShowItemCD }) => {
+  // console.log(data.itemCards[0].card, " RestaurantCategory", data);
+  // const [isActive, setIsActive] = useState(true);
 
   return (
     <div>
@@ -11,15 +11,16 @@ const RestaurantCategory = ({ data }) => {
       <div
         className="w-6/12 mx-auto p-3 bg-gray-50 shadow-md my-5 flex justify-between"
         onClick={() => {
-          setIsActive(!isActive);
+          setShowItemCD()
+          console.log(showItem)
         }}
       >
         <span>{data.title}</span>
-        <span> {isActive ? "-" : "+"} </span>
+        <span> {showItem ? "-" : "+"} </span>
       </div>
       {/* discription */}
       <div>
-        {isActive &&
+        {showItem &&
           data.itemCards.map((val) => (
             <Menu key={val?.card?.info?.id} itemDetails={val?.card} />
           ))}
